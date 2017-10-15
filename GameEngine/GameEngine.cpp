1da5dd76c74190ea116765f8134aaf1e4f364da1
MounirTakieddine <MounirTakieddine@bitbucket.org>
2017-10-15 01:02:07 +0100

    Set up GLEW 2.1.0 and GLFW 3.2.1 project dependencies. Updated executable files

--- GameEngine/GameEngine.cpp
+++ GameEngine/GameEngine.cpp
@@ -1,6 +1,59 @@
 #include "stdafx.h"
 
+enum ErrorCode
+{
+	ERROR_SUCCESS, 
+	ERROR_GLFW_INIT,
+	ERROR_WND_INIT,
+	ERROR_GLEW_INIT
+};
+
+static void OnWindowResized(int width, int height, GLFWwindow * window)
+{
+	glViewport(0, 0, width, height);
+}
+
 int main()
 {
-    return 0;
+
+	if (glfwInit() != GL_TRUE)
+	{
+		return ERROR_GLFW_INIT;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 4);
+	glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);
+
+	GLFWwindow * window = glfwCreateWindow(640, 480, "GameEngine", nullptr, nullptr);
+
+	if (!window)
+	{
+		return ERROR_WND_INIT;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK)
+	{
+		return ERROR_GLEW_INIT;
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClear(GL_COLOR_BUFFER_BIT);
+		glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, GL_TRUE);
+		}
+	}
+
+	glfwTerminate();
+
+    return ERROR_SUCCESS;
 }
